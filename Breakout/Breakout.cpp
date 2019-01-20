08d7b245cfc6e2e42154c857eeca526d2b6ff685
Mohammedwich <45858931+Mohammedwich@users.noreply.github.com>
2019-01-20 14:27:46 -0600

    Made the moving pad on the bottom of the screen

--- Breakout/Breakout.cpp
+++ Breakout/Breakout.cpp
@@ -6,13 +6,40 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
-
+using std::cin;
+using std::cout;
+using std::endl;
 
 
 int main()
 {
-	sf::RenderWindow mainWindow(sf::VideoMode(600, 900), "Breakout", sf::Style::Default);
-	sf::VertexArray ballDeflecter;
+	sf::ContextSettings context;
+	context.antialiasingLevel = 8;
+
+	sf::RenderWindow mainWindow(sf::VideoMode(550, 600), "Breakout", sf::Style::Default, context);
+
+	sf::Texture galaxyTexture;
+	if (!galaxyTexture.loadFromFile("Galaxy.jpg"))
+	{
+		cout << "Failed to load for galaxyTexture" << endl;
+	}
+
+	
+
+	sf::ConvexShape ballDeflector;
+	ballDeflector.setPointCount(6);
+	ballDeflector.setPoint(0, sf::Vector2f(225.f, 590.f));
+	ballDeflector.setPoint(1, sf::Vector2f(237.f, 582.f));
+	ballDeflector.setPoint(2, sf::Vector2f(253.f, 577.f));
+	ballDeflector.setPoint(3, sf::Vector2f(295.f, 577.f));
+	ballDeflector.setPoint(4, sf::Vector2f(313.f, 582.f));
+	ballDeflector.setPoint(5, sf::Vector2f(325.f, 590.f));
+
+	ballDeflector.setFillColor(sf::Color::Yellow);
+	ballDeflector.setTexture(&galaxyTexture);
+	ballDeflector.setTextureRect(sf::IntRect(145, 180, 250, 200));
+
+
 
 	while (mainWindow.isOpen())
 	{
@@ -25,14 +52,29 @@ int main()
 				mainWindow.close();
 			}
 
+			//Move the ballDeflector
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Left)
+				{
+					ballDeflector.move(-5, 0);
+				}
+
+				if (event.key.code == sf::Keyboard::Right)
+				{
+					ballDeflector.move(5, 0);
+				}
+			}
+
 
 		}
 
+		mainWindow.clear(sf::Color::Black);
+		mainWindow.draw(ballDeflector);
+		mainWindow.display();
 	}
 
-	mainWindow.clear(sf::Color::Black);
-	//mainWindow.draw(ballDeflector);
-	mainWindow.display();
+	
 
 
 	return 0;
