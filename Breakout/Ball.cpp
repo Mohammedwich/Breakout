49cf23211bc2e3065b2b3a1fe871501a6477b22c
Mohammedwich <45858931+Mohammedwich@users.noreply.github.com>
2019-01-23 23:43:09 -0600

    Made the ball and its physics. Does not interact with bricks yet. Also added start, pause, lose screens.

---
+++ Breakout/Ball.cpp
@@ -0,0 +1,61 @@
+#include "stdafx.h"
+#include "Ball.h"
+
+int Ball::numberOfBalls = 0;
+
+Ball::Ball()
+{
+	std::uniform_int_distribution<int> startingAngleDist(45, 135);	//Launch angle from deflector
+	
+	itsSpeed = 0.25f;
+	stuck = true;
+	int randomAngle = startingAngleDist(ballRanDev);
+	itsAngle = randomAngle * (2 * std::_Pi / 360);
+
+	sf::CircleShape::setRadius(10);
+	sf::CircleShape::setPointCount(7);
+	sf::CircleShape::setFillColor(sf::Color(104, 47, 0));
+	sf::CircleShape::setOrigin(getRadius(), getRadius());
+
+	++numberOfBalls;
+}
+
+
+Ball::~Ball()
+{
+}
+
+bool Ball::isStuck()
+{
+	return stuck;
+}
+
+void Ball::stick()
+{
+	stuck = true;
+}
+
+void Ball::unStick()
+{
+	stuck = false;
+}
+
+float Ball::getSpeed()
+{
+	return itsSpeed;
+}
+
+void Ball::setSpeed(float multiple)
+{
+	itsSpeed *= multiple;
+}
+
+long double Ball::getAngle()
+{
+	return itsAngle;
+}
+
+void Ball::setAngle(long double direction)
+{
+	itsAngle = direction;
+}
